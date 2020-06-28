5733f6465b2af98ed3a73298c8e20bc217979ac3
Evandro Augusto Guaranha Da Silva <evandroags@hotmail.com>
2020-06-28 01:36:19 -0300

    fourty commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Despesas Pessoais',
+      title: 'Despesas Pessoais - Flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
